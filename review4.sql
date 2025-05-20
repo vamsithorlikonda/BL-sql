79e9a561873a7349b72be92d62a0b112bb430e1b
vamsi <vamsithorlikonda99@gmail.com>
2025-05-21 00:31:09 +0530

    Deleted <filename>

--- review4.sql
+++
@@ -1,8 +0,0 @@
-SELECT m.season,m.venue,d.bowler,
-SUM(d.total_runs) AS total_runs
-FROM deliveries d
-JOIN matches m
-ON d.match_id = m.id
-GROUP BY m.season,m.venue,d.bowler
-HAVING SUM(d.total_runs) > 150
-ORDER BY m.season, m.venue, total_runs DESC;
