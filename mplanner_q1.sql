b2b0b98f503d5b542ce1a248013ec9bf06738961
vamsi <vamsithorlikonda99@gmail.com>
2025-05-20 15:59:19 +0530

    [Vamsi] Makerplanner question1

--- mplanner_q1.sql
+++ mplanner_q1.sql
@@ -1,24 +1,13 @@
-WITH global_stats AS (
-    SELECT 
-        SUM("deaths") AS global_deaths,
-        SUM("confirmed") AS global_confirmed
-    FROM country_wise_lastest
+#To find out the death percentage locally and globally
+
+with local_per as (
+select (Deaths * 100/Confirmed) as local_death_percentage from country_wise_latest
+where `Country/Region`='US'
 ),
-usa_stats AS (
-    SELECT 
-        SUM("Deaths") AS usa_deaths,
-        SUM("Confirmed") AS usa_confirmed
-    FROM usa_country_wise
-),
-local_stats AS (
-    SELECT 
-        (g.global_deaths - u.usa_deaths) AS local_deaths,
-        (g.global_confirmed - u.usa_confirmed) AS local_confirmed
-    FROM global_stats g, usa_stats u
+global_per as (
+select `Country/Region`,(Deaths * 100/Confirmed) as global_death_percentage from country_wise_latest
+where `Country/Region`!='US'
 )
-SELECT 
-    (local_stats.local_deaths::decimal / NULLIF(local_stats.local_confirmed, 0)) * 100 AS local_death_percentage,
-    (g.global_deaths::decimal / NULLIF(g.global_confirmed, 0)) * 100 AS global_death_percentage
-FROM 
-    local_stats,
-    global_stats g;
+
+select * from global_per,local_per;
+
